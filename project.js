688f35317f6d8da68bc72f9e4a4698975a92f4bb
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-19 17:52:39 +0200

    Project Plan

---
+++ project.js
@@ -0,0 +1,8 @@
+//STEPS FOR MY PROJECT
+//1. Deposit some money
+//2. Determine number of lines to bet on
+//3. Collect a bet amount
+//4. Spin the slot machine
+//5. Check if the user won
+//6. Give the user ther winnings
+//7. Play again
