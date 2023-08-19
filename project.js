e69efc668ce8cd6059ba6fcb2f60a07753d82773
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-19 22:49:18 +0200

    Collect the bet amount

--- project.js
+++ project.js
@@ -19,5 +19,32 @@ const deposit = () =>{ //Declaring constant function
     }//Close else block
   }//End while loop
 };
-const depositAmount= deposit();
-console.log(depositAmount);
+
+const getNumberOfLines = () => {
+  while(true){ //Initiate an infinite loop considering the condition is true
+    const lines = prompt("Enter a number Of Lines to bet on (0-3): ");//Prompt message and waits for the user to input a value
+    const numberOfLines = parseFloat(lines);//Converting user input to a floating-point number
+
+    if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){ //Checks if the value provided is not a valid number
+      console.log("Invalid number of lines entered, try again."); //If the if statements evaluates to True this line prints
+    }else{//If the user puts a number greater than 0
+      return numberOfLines; //Returns the valid lines
+    }//Close else block
+  }//End while loop
+};
+
+const getBet = (balance, lines) => {
+  while(true){ //Initiate an infinite loop considering the condition is true
+    const bet = prompt("Enter the total bet per line: ");//Prompt message and waits for the user to input a value
+    const numberBet = parseFloat(bet);//Converting user input to a floating-point number
+
+    if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines){ //Checks if the value provided is not a valid number
+      console.log("Invalid bet, try again."); //If the if statements evaluates to True this line prints
+    }else{//If the user puts a number greater than 0
+      return numberBet; //Returns the valid lines
+    }//Close else block
+  }//End while loop
+};
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
